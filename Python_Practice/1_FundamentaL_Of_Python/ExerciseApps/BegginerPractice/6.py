a09fcf86c0a5cbc9abdede9d846b6ea03b315698
ziejaCode <ziejaCode@gmail.com>
2021-03-17 18:18:52 +0000

    commit

--- Python_Practice/1_FundamentaL_Of_Python/ExerciseApps/BegginerPractice/6.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on 31 May 2020
-
-@author: czarn
-'''
-p1 = input()
-p2 = input()
-
--- Python_Practice/1_FundamentaL_Of_Python/ExerciseApps/oddOrEven.py
+++
@@ -1,7 +0,0 @@
-
-num = input("Whats your number? ")
-
-if int(num)%2:
-    print("This num is odd")
-else:
-    print("This num is even")
